46e72abe47911c1a51974345c29840eca3d2ab14
jinnoxify <oscarpereznavarro1997@gmail.com>
2020-08-05 17:46:55 -0500

    Updates

---
+++ precache-manifest.601a15880b9018e205ba6ea4e989e0a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d45b2f403b7f3152bb9ecea56859a847",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "c6fc137c74493e97a9a5",
+    "url": "/pomodoro-clock/static/css/main.80f1adc5.chunk.css"
+  },
+  {
+    "revision": "018e254475ad8678da63",
+    "url": "/pomodoro-clock/static/js/2.19693eeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.19693eeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fc137c74493e97a9a5",
+    "url": "/pomodoro-clock/static/js/main.7fe450ee.chunk.js"
+  },
+  {
+    "revision": "01b85f1bd2679b968b61",
+    "url": "/pomodoro-clock/static/js/runtime-main.32eba626.js"
+  }
+]);
